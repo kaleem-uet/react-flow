afb0d383334820574f635abf2da560a6081ed94b
kaleem ullah <71625133+kaleem-uet@users.noreply.github.com>
2023-10-05 20:51:34 +0500

    ustom node

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,95 @@
+import { useCallback, useState } from "react";
 import ReactFlow, {
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
   Background,
   Controls,
   MiniMap,
-  NodeToolbar,
-  Panel,
 } from "reactflow";
 import "reactflow/dist/style.css";
-import { useNodesState, useEdgesState, addEdge } from "reactflow";
-import { useCallback, useState } from "react";
+
+// import TextUpdaterNode from "./TextUpdaterNode.js";
+
+import "./text-updater-node.css";
+import TextUpdaterNode from "./TextUpdaterNode";
+
+const rfStyle = {
+  backgroundColor: "#B8CEFF",
+};
 
 const initialNodes = [
   {
-    id: "1",
+    id: "node-1",
+    type: "textUpdater",
     position: { x: 0, y: 0 },
-    data: { label: "1" },
-    style: { backgroundColor: "#6ede87", color: "white" },
+    data: { value: 123 },
   },
   {
-    id: "2",
-    position: { x: 0, y: 100 },
-    data: { label: "2" },
-    style: { backgroundColor: "#6865A5", color: "white" },
+    id: "node-2",
+    type: "input",
+    targetPosition: "top",
+    position: { x: 0, y: 200 },
+    data: { label: "node 2" },
   },
   {
-    id: "3",
-    position: { x: 0, y: 150 },
-    data: { label: "3" },
-    style: { backgroundColor: "#ff0072", color: "white" },
+    id: "node-3",
+    type: "output",
+    targetPosition: "top",
+    position: { x: 200, y: 200 },
+    data: { label: "node 3" },
+  },
+  {
+    id: "node-4",
+    type: "output",
+    targetPosition: "top",
+    position: { x: 500, y: 500 },
+    data: { label: "node 4" },
   },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2", animated: true }];
-// add interectivity to the nodes
 
-function App() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const [variant, setVariant] = useState("cross");
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
+const initialEdges = [
+  { id: "edge-1", source: "node-1", target: "node-2", sourceHandle: "a" },
+  { id: "edge-2", source: "node-1", target: "node-3", sourceHandle: "b" },
+];
+
+// we define the nodeTypes outside of the component to prevent re-renderings
+// you could also use useMemo inside the component
+const nodeTypes = { textUpdater: TextUpdaterNode };
+
+function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
     [setEdges]
   );
-  const nodeColor = (node) => {
-    switch (node.type) {
-      case "input":
-        return "#6ede87";
-      case "output":
-        return "#6865A5";
-      default:
-        return "#ff0072";
-    }
-  };
+  const onConnect = useCallback(
+    (connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-      >
-        <Panel>
-          <div>variant:</div>
-          <button onClick={() => setVariant("dots")}>dots</button>
-          <button onClick={() => setVariant("lines")}>lines</button>
-          <button onClick={() => setVariant("cross")}>cross</button>
-        </Panel>
-        <Controls />
-        <MiniMap nodeColor={nodeColor} nodeStrokeWidth={3} zoomable pannable />
-        <Background variant={variant} gap={12} size={1} />
-      </ReactFlow>
-    </div>
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      nodeTypes={nodeTypes}
+      fitView
+      style={rfStyle}
+    >
+      <Background />
+      <Controls />
+      <MiniMap />
+    </ReactFlow>
   );
 }
 
-export default App;
+export default Flow;
