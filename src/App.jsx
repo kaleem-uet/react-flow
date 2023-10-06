8fa01c851325605f9983aaf0a1d3eb16a0aec935
kaleem ullah <71625133+kaleem-uet@users.noreply.github.com>
2023-10-06 17:47:08 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,79 @@
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import ReactFlow, {
-  addEdge,
-  applyEdgeChanges,
-  applyNodeChanges,
   Background,
   Controls,
   MiniMap,
+  ReactFlowProvider,
+  useReactFlow,
 } from "reactflow";
 import "reactflow/dist/style.css";
 
-import initialNodes from "./nodes";
-import initialEdges from "./edges";
+import defaultNodes from "./nodes2";
+import defaultEdges from "./edges2";
 
-const rfStyle = {
-  backgroundColor: "#D0C0F7",
+const edgeOptions = {
+  animated: true,
+  style: {
+    stroke: "white",
+  },
 };
 
-function Flow() {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+const connectionLineStyle = { stroke: "white" };
 
-  const onNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-    [setNodes]
-  );
-  const onEdgesChange = useCallback(
-    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    [setEdges]
-  );
-  const onConnect = useCallback(
-    (connection) => setEdges((eds) => addEdge(connection, eds)),
-    [setEdges]
-  );
+let nodeId = 0;
+
+function Flow() {
+  const reactFlowInstance = useReactFlow();
+  const onClick = useCallback(() => {
+    const id = `${++nodeId}`;
+    const newNode = {
+      id,
+      position: {
+        x: Math.random() * 500,
+        y: Math.random() * 500,
+      },
+      data: {
+        label: `Node ${id}`,
+      },
+    };
+    reactFlowInstance.addNodes(newNode);
+  }, []);
 
   return (
     <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
+      defaultNodes={defaultNodes}
+      defaultEdges={defaultEdges}
+      defaultEdgeOptions={edgeOptions}
       fitView
-      style={rfStyle}
-      attributionPosition="top-right"
+      style={{
+        backgroundColor: "#D3D2E5",
+      }}
+      connectionLineStyle={connectionLineStyle}
     >
-      <Background />
+      <button
+        onClick={onClick}
+        style={{
+          position: "absolute",
+          top: 10,
+          left: 10,
+          zIndex: 10,
+          padding: 10,
+          backgroundColor: "#FFA500",
+        }}
+      >
+        add node
+      </button>
       <Controls />
       <MiniMap />
+      <Background />
     </ReactFlow>
   );
 }
 
-export default Flow;
+export default function () {
+  return (
+    <ReactFlowProvider>
+      <Flow />
+    </ReactFlowProvider>
+  );
+}
